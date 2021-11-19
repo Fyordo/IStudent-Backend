e5bcfea4839dc9c2c5cf9679e970cd08ea9c6d3c
Fedor Lagutin <lagutinfedya@gmail.com>
2021-11-19 21:31:39 +0300

    Подключение АПИ Расписания: Добавлена таблица уровней образования

---
+++ database/migrations/2021_11_19_181510_create_grades_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->integer("num");
+            $table->string("degree");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
