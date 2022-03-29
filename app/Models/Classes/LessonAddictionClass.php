91fe2347f317339585795f6389977f1819eff9ec
Stanislav Leonchik <leonstas1@mail.ru>
2022-03-29 19:34:17 +0300

    Добавлена подгрузка урока из бд

--- app/Models/Classes/LessonAddictionClass.php
+++ app/Models/Classes/LessonAddictionClass.php
@@ -2,19 +2,22 @@
 
 namespace App\Models\Classes;
 
+use App\Models\Lesson;
 use DateTime;
 
 class LessonAddictionClass
 {
     public int $id;
-    public int $lesson_id;
+//    public int $lesson_id;
     public DateTime $date;
     public string $description;
+    public LessonClass $lesson;
 
     public function __construct($arr)
     {
         $this->id = $arr["id"];
-        $this->lessonId = $arr["lesson_id"];
+//        $this->lessonId = $arr["lesson_id"];
+        $this->lesson = Lesson::where('id',$arr["lesson_id"])->first();
         $this->date = $arr["date"];
         $this->description = $arr["description"];
     }
